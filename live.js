0d808e4c4f384894138960994dd43154512b1327
pritam <myinfo.majumder@gmail.com>
2020-01-01 18:05:14 +0600

    run a line in console hahaha

---
+++ live.js
@@ -0,0 +1,13 @@
+//This 2020 new year Statistics :)
+let old = 2019;
+let twentytwenty = 2020;
+let constantLearning = 'agree';
+function newYear(hardWork) {
+    if(hardWork == constantLearning){
+        console.log(twentytwenty + 'Is the year of' + ' smilling :) * + Learnig * + Savings *')
+    }else {
+        console.log(old + ' and 2020 is Error!');
+    }
+}
+
+newYear('agree');
