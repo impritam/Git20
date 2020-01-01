374ee51f2911888820370b7fa567bb1964bf5526
pritam <myinfo.majumder@gmail.com>
2020-01-01 18:39:15 +0600

    branchOne we edited some line!

--- live.js
+++ live.js
@@ -3,7 +3,7 @@ let twentynineteen = 2019;
 let twentytwenty = 2020;
 let constantLearning = 'agree';
 function newYear(hardWork) {
-    if(hardWork == constantLearning){
+    if(hardWork === constantLearning){
         console.log(twentytwenty + 'Is the year of' + ' smilling :) * + Learnig * + Savings *')
     }else {
         console.log(old + ' and 2020 is Error!');
